80b923cf559a7ec2b4d9eaec7a3f2084be9de090
muokapwambua <muokapwambua@gmail.com>
2021-04-19 12:24:46 +0300

    fix + virtual added

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from dateutil.relativedelta import relativedelta
 app = Flask(__name__)
 
 
-@app.route('/generatetoken/<u_id>/<time>')
-def token_generator(u_id, time):
+@app.route('/generatetoken/<u_id>')
+def token_generator(u_id):
   
-  exp = dt.utcnow() + relativedelta(time=+1)
+  exp = dt.utcnow() + relativedelta(days=+1)
   
   if u_id:
     token = jwt.encode({"id": u_id, "exp": exp}, str("SECRET_KEY"))
-    return {'token': token.decode("UTF-8"), 'id': u_id, 'expire': exp}
+    return {'token': token, 'id': u_id, 'expire': exp}
   return None    
   
   
